42a050176813a6d2788aa82efca5bfd7ef0e7fc5
Mike-chege <mikechege93@gmail.com>
2023-12-28 12:34:47 +0000

    Returning a dictionally containing several key-value pairs

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Hypermedia pagination
+"""
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """
+    Class Server
+    Paginates a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns a list of baby names
+        Or
+        Asserts an error
+        """
+        assert(isinstance(page, int) and isinstance(page_size, int))
+        assert(page > 0 and page_size > 0)
+        [start, end] = index_range(page, page_size)
+        return self.dataset()[start: end]
+
+    def get_hyper(self, page: int, page_size: int) -> dict:
+        """
+        Retuns a dictionary
+        """
+        dataset_records = self.get_page(page, page_size)
+
+        page_dict = math.ceil(len(self.__dataset) / page_size)
+
+        return {
+            'page_size': len(dataset_records),
+            'page': page,
+            'data': dataset_records,
+            'next_page': page + 1 if (page + 1) <= page_dict else None,
+            'prev_page': page - 1 if (page - 1) > 0 else None,
+            'total_pages': page_dict
+        }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index,
+    And an end index corresponding to the range of indexes,
+    To return in a list or those particular pagination,
+    Parameters
+    """
+    return ((page - 1) * page_size, page * page_size)
