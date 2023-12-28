ed8f30ea1c6bd7a04e49bfd3d07f1d38f5ff2062
Mike-chege <mikechege93@gmail.com>
2023-12-28 12:51:51 +0000

    Used assert to verify index is in valid range

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -44,8 +44,21 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Get hyper_index
+        Takes two integer arguments
         """
-        pass
+        assert type(index) == int and type(page_size) == int
+        assert 0 <= index < len(self.indexed_dataset())
+        pages = []
+        next_index = index + page_size
+        for i in range(index, index + page_size):
+            if not self.indexed_dataset().get(i):
+                i += 1
+                next_index += 1
+            pages.append(self.indexed_dataset()[i])
+        return {'index': index,
+                'next_index': next_index,
+                'page_size': page_size,
+                'data': pages
+                }
