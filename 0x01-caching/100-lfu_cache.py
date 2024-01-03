1c1eaaabb22c2cb69ec20abdf24db74651fb36b7
Mike-chege <mikechege93@gmail.com>
2024-01-03 10:05:40 +0000

    Implementing LFU Caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+LFU  Caching
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LFUCache(BaseCaching):
+    """
+    Class LFUCache
+    Definition
+    """
+    def __init__(self):
+        """
+        Initialize the class instance
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.mru = ""
+
+    def put(self, key, item):
+        """
+        Updates an item
+        In the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+                else:
+                    # discard the most recently used item
+                    discarded = self.mru
+                    del self.cache_data[discarded]
+                    print("DISCARD: {}".format(discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
+            else:
+                self.cache_data[key] = item
+                self.mru = key
+
+    def get(self, key):
+        """
+        Gets an
+        Item by key
+        """
+        if key in self.cache_data:
+            self.mru = key
+            return self.cache_data[key]
