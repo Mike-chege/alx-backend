83ec782e28ba02ab8e253b6a9c059a836f2398d9
Mike-chege <mikechege93@gmail.com>
2024-01-09 12:43:20 +0000

    Instantiating Babel object

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Instatiating Babel object
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+babel = Babel(app)
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+    Configure available languages for the app
+    """
+    LANGUAGES = ['en', 'fr']
+    # Inherent defaults
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# Sets class config as the configuration for the app
+app.config.from_object('1-app.config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    Get / route then return 0-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
